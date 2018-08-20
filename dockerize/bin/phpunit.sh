ac77825a9a0617e76f398c5f657d7062f965e1c9
William Correa <wilcorrea@gmail.com>
2018-08-20 10:54:03 -0300

    Use phpunit alias to .dockerize.phpunit file

--- dockerize/bin/phpunit.sh
+++ dockerize/bin/phpunit.sh
@@ -2,10 +2,11 @@
 
 function phpunit
 {
-    T_CURRENT=$(basename $(dirname $(readlink -f ${0})))
-
+    T_CURRENT=$(dirname $(readlink -f ${0}))
     if [[ ! -f ${T_CURRENT}/.dockerize.phpunit ]]; then
+      echo "${red}# ${T_CURRENT} don't have a '.dockerize.phpunit' file ${reset}"
       return
     fi
-    source ${T_CURRENT}/.dockerize.phpunit
+    echo "${green}# Runing your '.dockerize.phpunit' file ${reset}"
+    ${T_CURRENT}/.dockerize.phpunit ${@}
 }
